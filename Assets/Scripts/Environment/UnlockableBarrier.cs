a1488c9e5659289f9aadb6652715ea9dfb5fc5bd
Sara Hernández Suárez <sara.her.su@gmail.com>
2023-07-05 13:01:59 +0100

    fix: control multiple collissions on barrier script

--- Assets/Scripts/Environment/UnlockableBarrier.cs
+++ Assets/Scripts/Environment/UnlockableBarrier.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class UnlockableBarrier : MonoBehaviour
 {
     bool currentlyLocked = true;
-    float currentlyActiveButtons = 0;
+    Dictionary<int, int> currentlyActiveButtons;
     public List<ActionableButton> buttonsRequiredToUnlock;
 
     // Start is called before the first frame update
@@ -16,6 +16,8 @@ public class UnlockableBarrier : MonoBehaviour
             Debug.LogWarning("No buttons have been specified to open this barrier, so it cannot be unlocked");
             return;
         }
+
+        currentlyActiveButtons = new Dictionary<int, int>();
 
         ActionableButton.OnButtonPress += OnButtonPress;
         ActionableButton.OnButtonRelease += OnButtonRelease;
@@ -30,23 +32,40 @@ public class UnlockableBarrier : MonoBehaviour
 
         // Disable barrier so it's no longer visible and characters can move forward
         this.gameObject.SetActive(false);
+        currentlyLocked = false;
     }
 
     void OnButtonPress (ActionableButton button) {
         if (buttonsRequiredToUnlock.Contains(button)) {
-            Debug.Log(currentlyActiveButtons + " active buttons for " + this.gameObject.name);
-            currentlyActiveButtons ++;
+            UpdateCurrentActiveButtonState(button, 1);
+            Debug.Log(currentlyActiveButtons.Count + " active buttons for " + this.gameObject.name);
         }
 
-        if (currentlyLocked && currentlyActiveButtons == buttonsRequiredToUnlock.Count) {
+        // Unlock barrier only when all buttons are active at the same time
+        if (currentlyLocked && currentlyActiveButtons.Count == buttonsRequiredToUnlock.Count) {
             UnlockBarrier();
         }
     }
 
     void OnButtonRelease (ActionableButton button) {
         if (buttonsRequiredToUnlock.Contains(button)) {
-            Debug.Log(currentlyActiveButtons + " active buttons for " + this.gameObject.name);
-            currentlyActiveButtons--;
+            UpdateCurrentActiveButtonState(button, -1);
+            Debug.Log(currentlyActiveButtons.Count + " active buttons for " + this.gameObject.name);
+        }
+    }
+
+    // In case multiple elements are overlapping with the button, make sure we don't delete it from dictionary until all collisions have ended
+    void UpdateCurrentActiveButtonState (ActionableButton button, int increase) {
+        int buttonId = button.GetInstanceID();
+        if (currentlyActiveButtons.ContainsKey(buttonId)) {
+            int activeCollisionCount = currentlyActiveButtons[buttonId] + increase;
+            if (activeCollisionCount == 0) {
+                currentlyActiveButtons.Remove(buttonId);
+            } else {
+                currentlyActiveButtons[buttonId] = activeCollisionCount;
+            }
+        } else if (increase > 0) {
+            currentlyActiveButtons.Add(buttonId, increase);
         }
     }
 }
